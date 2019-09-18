719041b690c6b45c0bee4506b2d3797305b3a728
martxn <abadmartin.ma@gmail.com>
2019-09-18 10:32:46 -0300

    Busca numeros primos

---
+++ primo.py
@@ -0,0 +1,16 @@
+end = 10000
+pr = 0
+x = 1
+
+
+while pr <= end:
+  x += 1
+  for k in range (2, x//2):
+     if x %k == 0:
+        break
+  else:
+#     print (x)
+     pr += 1
+
+print (pr)
+print (x)
